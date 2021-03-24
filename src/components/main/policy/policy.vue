085ee56e411a3f8721b567579fd57a27e836437f
jjggzz <1945282561@qq.com>
2021-03-24 20:12:08 +0800

    init

--- src/components/main/policy/policy.vue
+++ src/components/main/policy/policy.vue
@@ -136,7 +136,7 @@
                             :before-remove="onRemove"
                             multiple>
                         <el-button size="small" type="primary" >点击上传</el-button>
-                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                        <div slot="tip" class="el-upload__tip">只能上传不超过500MB的文件</div>
                     </el-upload>
                 </el-form-item>
             </el-form>
